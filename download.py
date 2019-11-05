740ca57d5c6c542acfee2fd02545fcdccf02bf2b
Nauman Mustafa <nauman.mustafa.x@gmail.com>
2019-11-05 15:24:11 +0500

    Medium model (345M) cannot run on Cloud Run

--- download.py
+++ download.py
@@ -2,10 +2,10 @@
 from pytorch_transformers.file_utils import cached_path
 
 urls = [
-    'https://s3.amazonaws.com/models.huggingface.co/bert/gpt2-medium-vocab.json',
-    'https://s3.amazonaws.com/models.huggingface.co/bert/gpt2-medium-merges.txt',
-    'https://s3.amazonaws.com/models.huggingface.co/bert/gpt2-medium-pytorch_model.bin',
-    'https://s3.amazonaws.com/models.huggingface.co/bert/gpt2-medium-config.json'
+    'https://s3.amazonaws.com/models.huggingface.co/bert/gpt2-vocab.json',
+    'https://s3.amazonaws.com/models.huggingface.co/bert/gpt2-merges.txt',
+    'https://s3.amazonaws.com/models.huggingface.co/bert/gpt2-pytorch_model.bin',
+    'https://s3.amazonaws.com/models.huggingface.co/bert/gpt2-config.json'
 ]
 
 for url in urls:
