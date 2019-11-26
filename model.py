08f1cf3998cd643cd4532c8a09c2b4542fb203ff
Nauman Mustafa <nauman.mustafa.x@gmail.com>
2019-11-26 13:06:46 +0500

    fix bug for model loading + add support for GPU

--- model.py
+++ model.py
@@ -3,13 +3,15 @@ from torch.nn import functional as F
 from pytorch_transformers import GPT2Tokenizer, GPT2Model, GPT2LMHeadModel
 torch.set_grad_enabled(False)
 
-tokenizer = GPT2Tokenizer.from_pretrained('gpt2-medium')
-model = GPT2LMHeadModel.from_pretrained('gpt2-medium').eval()
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+model = GPT2LMHeadModel.from_pretrained('gpt2').eval()
+model = model.to(device)
 
 
 def extend(text, size=20):
     tokens = tokenizer.encode(text)
-    prediction, past = torch.tensor([tokens]), None
+    prediction, past = torch.tensor([tokens]).to(device), None
     for i in range(size):
         prediction, past = model(prediction, past=past)
         prediction = torch.multinomial(F.softmax(prediction[:, -1]), 1)
