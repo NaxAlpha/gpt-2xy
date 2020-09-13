07f43de5dda9121fbe09549863f63735b3ac70ee
Nauman Mustafa <nauman.mustafa.x@gmail.com>
2020-09-13 11:49:45 +0500

    upgrade to gpt-2 medium

--- download.py
+++ download.py
@@ -1,6 +1,8 @@
 # Downloads model(s) during docker build
 from transformers import GPT2LMHeadModel, GPT2Tokenizer
 
-GPT2LMHeadModel.from_pretrained('gpt2').save_pretrained('./gpt2')
-GPT2Tokenizer.from_pretrained('gpt2').save_pretrained('./gpt2')
+model_name = 'gpt2-medium'
+
+GPT2LMHeadModel.from_pretrained(model_name).save_pretrained('./gpt2')
+GPT2Tokenizer.from_pretrained(model_name).save_pretrained('./gpt2')
 
