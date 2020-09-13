7f6c8812e68d5405e7dcbd204da8192e4da3cf92
Nauman Mustafa <nauman.mustafa.x@gmail.com>
2020-09-13 12:08:48 +0500

    switch to distilgpt2 for better performance

--- download.py
+++ download.py
@@ -1,7 +1,7 @@
 # Downloads model(s) during docker build
 from transformers import GPT2LMHeadModel, GPT2Tokenizer
 
-model_name = 'gpt2-medium'
+model_name = 'distilgpt2'
 
 GPT2LMHeadModel.from_pretrained(model_name).save_pretrained('./gpt2')
 GPT2Tokenizer.from_pretrained(model_name).save_pretrained('./gpt2')
